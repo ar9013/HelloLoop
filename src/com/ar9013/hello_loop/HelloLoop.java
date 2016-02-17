627acb721367a7b3f7e124eccbf2c1536951b4a1
Peter <peter@honixtech.com>
2016-02-17 13:44:09 +0800

    HelloLoop

---
+++ src/com/ar9013/hello_loop/HelloLoop.java
@@ -0,0 +1,36 @@
+package com.ar9013.hello_loop;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+
+public class HelloLoop extends SimpleApplication{
+
+	public static void main(String[] args) {
+		HelloLoop app = new HelloLoop();
+        app.start();
+	}
+	
+	protected Geometry player;
+	
+	@Override
+	public void simpleInitApp() {
+		/** this blue box is our player character */
+		Box b = new Box(1, 1, 1);
+		player = new Geometry("blue box", b);
+		Material mat = new Material(assetManager,"Common/MatDefs/Misc/Unshaded.j3md");
+		mat.setColor("Color", ColorRGBA.Blue);
+		player.setMaterial(mat);
+		rootNode.attachChild(player);
+		
+	}
+
+	 /* Use the main event loop to trigger repeating actions. */
+	@Override
+	public void simpleUpdate(float tpf) {
+		// make the player rotate:
+        player.rotate(0, 2*tpf, 0); 
+	}
+}
